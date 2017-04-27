47738c97f32fa962700e50b593327337ef71a5eb
Rafael Neves <mr.karto1000@hotmail.com>
2017-04-27 18:08:56 -0300

    Create exerc_2.java

---
+++ exerc_2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner:
+  class exerc_2{
+    
+    /*This tutorial we do a simple addition usin variables pre setted with a value*/
+    
+    public static void main (String args[]){
+      
+      Scanner entrada = new Scanner(System.in);
