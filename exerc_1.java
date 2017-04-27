fdcc350c5c8c34acbaf8726e730dbf6d9ccef14f
Rafael Neves <mr.karto1000@hotmail.com>
2017-04-27 18:07:41 -0300

    Update exerc_1.java

--- exerc_1.java
+++ exerc_1.java
@@ -1,3 +1,30 @@
 import java.util.Scanner:
   class exerc_1{
-  }
+    
+    /*This tutorial we do a simple calculator*/
+    
+    public static void main (String args[]){
+      
+      Scanner entrada = new Scanner(System.in);
+
+//---------------Variable declarations-------------
+      
+      double fnum, snum, answer;
+      
+//---------------Data processing-------------------
+      
+      System.out.println("Enter first num: ");
+      fnum = entrada.nextDouble();
+      
+      System.out.println("Enter second num: ");
+      snum = entrada.nextDouble();
+      
+      answer = fnum + snum;
+      
+//---------------Data output------------------------
+      
+      System.out.println(answer);
+      
+    }// main mode end
+    
+  }//program end
