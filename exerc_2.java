c48a83ca06b52ffe124e0bc60bf27e3df63d160a
Rafael Neves <mr.karto1000@hotmail.com>
2017-04-27 18:12:33 -0300

    Update exerc_2.java

--- exerc_2.java
+++ exerc_2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner:
   class exerc_2{
     
-    /*This tutorial we do a simple addition usin variables pre setted with a value*/
+/*This tutorial we do a simple addition usin variables pre setted with a value*/
     
     public static void main (String args[]){
       
