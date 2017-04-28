50cb65d95a3d3f61e56c1dbbefe3a9adc28d93fc
Rafael Neves <mr.karto1000@hotmail.com>
2017-04-27 23:33:00 -0300

    Update exerc_2.java

--- exerc_2.java
+++ exerc_2.java
@@ -1,13 +1,14 @@
-import java.util.Scanner:
+import java.util.Scanner;
+
   class exerc_2{
     
 /*This tutorial we do a simple addition usin variables pre setted with a value*/
     
     public static void main (String args[]){
       
-      Scanner entrada = new Scanner(System.in);
-      
 //--------------Varibles declarations--------------------
+      
+      /* working with a simple math operator*/
       
       int girls, boys, people;
       
@@ -16,5 +17,49 @@ import java.util.Scanner:
       people = girls + boys;
       
       System.out.println(people);
-    }
-  }
+
+//--------------Variables declaration---------------------
+     
+      /*Working with division using int. If we have a decimal number as result 
+      of our division and we declarated as int the variable it will show us just
+      de integre part as result*/
+
+      
+      int girls2, boys2, people2;
+      
+      girls2 = 11;
+      boys2 = 3;
+      people2 = girls2 / boys2;
+      
+      System.out.println(people2);
+      
+//---------------Variables declarations----------------------
+      
+      /* with a float variable declareted all results of decimal numbers will be printed*/
+      
+      float girls3, boys3, people3;
+      
+      girls3 = 11;
+      boys3 = 3;
+      people3 = girls3 / boys3;
+      
+      System.out.println(people3);
+      
+//---------------Variables declarations----------------------
+      
+      /* with a modules operator (%) we show the left value of a division*/
+      
+      int girls4, boys4, people4;
+      
+      girls4 = 11;
+      boys4 = 3;
+      people4 = girls4 % boys4;
+      
+      System.out.println(people4);
+      
+    }//fim do metodo main
+    
+  }//fim do programa
+
+/* when we are using int in math operatos the result always will be a int
+*/
