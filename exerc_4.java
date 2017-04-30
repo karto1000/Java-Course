d0c486861a556d33bcc6ccf3ef84bdbe233171a3
Rafael Neves <mr.karto1000@hotmail.com>
2017-04-30 09:07:36 -0300

    Create exerc_4.java

---
+++ exerc_4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+  public class exerc_4{
+  
+  /* In this exercise we are working with de Switch Statement*/
+  
+    public static void main (String args[]){
+      Scanner entrada = new Scanner(System.in);
+      
+//---------------Variables DEclaration--------------      
+    int age = 3;
+    
+    }
+  
+  }
